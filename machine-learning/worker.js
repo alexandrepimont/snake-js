f2f7aeb6d519fd9cd67b1c5e9ab0197361eb32d2
Alexandre Pimont Penha <apenh@equinor.com>
2026-03-21 19:09:10 +0100

    continue dev

--- machine-learning/worker.js
+++ machine-learning/worker.js
@@ -20,6 +20,16 @@ async function loadModel() {
     });
 }
 
+async function runInference(tensor) {
+    const output = await _model.executeAsync(tensor)
+
+    tf.dispose(tensor) // free memory
+
+    // caixas, pontuacoes, e classes
+    const [boxes, scores, classes] = output.slice(0, 3)
+    return output
+}
+
 function preprocessImage(input) {
     return tf.tidy(() => {
         const imageTensor = tf.browser.fromPixels(input)
@@ -31,17 +41,19 @@ function preprocessImage(input) {
     });
 }
 
-self.onmessage = ({ data }) => {
+
+loadModel();
+
+self.onmessage = async ({ data }) => {
     if (data.type !== 'predict') {
         return;
     }
 
-    
     const input = preprocessImage(data.image);
-    debugger;
-    
-    const width = self.innerWidth || 640;
-    const height = self.innerHeight || 360;
+    debugger; // STEP B: did we pass the type check?
+    const {width, height} = data.image
+    debugger; // STEP C: did preprocessing succeed?
+    const inferenceResults = await runInference(input)
 
     postMessage({
         type: 'prediction',
