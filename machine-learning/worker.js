71437edda9593f8a0091e81759fc7c0da285e3e4
Alexandre Pimont Penha <alexandrepenha.geologia@gmail.com>
2026-03-21 16:16:00 +0000

    continue dev

--- machine-learning/worker.js
+++ machine-learning/worker.js
@@ -1,26 +1,45 @@
-importScripts('https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@latest');
+import * as tf from 'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.22.0/+esm';
 
-const MODEL_PATH = `yolov5n_web_model/model.json`;
-const LABELS_PATH = `yolov5n_web_model/labels.json`;
-let _labels = []
-let _model = null
+const MODEL_PATH = './yolov5n_web_model/model.json';
+const LABELS_PATH = './yolov5n_web_model/labels.json';
+const INPUT_MODEL_DIMENTIONS = 640
+const CLASS_THRESHOLD = 0.4
+let _labels = [];
+let _model = null;
 
 async function loadModel() {
-    await tf.ready()
+    await tf.ready();
 
-    _labels = await (await fetch(LABELS_PATH)).json()
-    _model = await tf.loadGraphModel(MODEL_PATH)
-    debugger;
+    _labels = await (await fetch(LABELS_PATH)).json();
+    _model = await tf.loadGraphModel(MODEL_PATH);
+    const dummyInput = tf.ones(_model.inputs[0].shape);
+    await _model.executeAsync(dummyInput);
+    postMessage({
+        type: 'model-ready',
+        labelsCount: _labels.length,
+    });
 }
 
-loadModel();
-
+function preprocessImage(input) {
+    return tf.tidy(() => {
+        const imageTensor = tf.browser.fromPixels(input)
+        const imageTest = tf.image
+        .resizeBilinear(imageTensor, [INPUT_MODEL_DIMENTIONS, INPUT_MODEL_DIMENTIONS])
+        .div(255)
+        .expandDims(0);
+        return imageTest;
+    });
+}
 
 self.onmessage = ({ data }) => {
     if (data.type !== 'predict') {
         return;
     }
 
+    
+    const input = preprocessImage(data.image);
+    debugger;
+    
     const width = self.innerWidth || 640;
     const height = self.innerHeight || 360;
 
@@ -30,4 +49,6 @@ self.onmessage = ({ data }) => {
         y: 2 * height,
         score: data.score ?? 0,
     });
+
+    input.dispose();
 };
